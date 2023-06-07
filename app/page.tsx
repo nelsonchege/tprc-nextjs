da289edff7e42b60a22e2b62d4f6befc6930708f
nelson chege <nelson3chege@gmail.com>
2023-06-08 00:19:38 +0300

    update:finish up on skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,111 @@
-'use client';
+"use client";
 
-import { trpc } from '@/utils/trpc'
-import { Card } from 'primereact/card';
-import { useState } from 'react';
-import { Skeleton } from "@/components/ui/skeleton"
-import "primereact/resources/themes/lara-light-indigo/theme.css";     
+import { trpc } from "@/utils/trpc";
+import { Card } from "primereact/card";
+import { useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
-import React from 'react';
+import React from "react";
 
 export default function Home() {
-  const [name ,setName] =useState('')
-  const [url ,setUrl] =useState('')
-  const [description ,setDescription] =useState('')
+  const [name, setName] = useState("");
+  const [url, setUrl] = useState("");
+  const [description, setDescription] = useState("");
   const mutation = trpc.saveBrand.useMutation();
-  const {data,isFetching} = trpc.userById.useQuery()
-  const handleSubmit = async (e: { preventDefault: () => void; }) => {
-    mutation.mutate({ name,url,description });
-    setName('')
-    setUrl('')
-    setName('')
-  }
-  if (isFetching) return <Loading/>
+  const { data, isFetching } = trpc.userById.useQuery();
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    mutation.mutate({ name, url, description });
+    setName("");
+    setUrl("");
+    setName("");
+  };
+  if (isFetching) return <Loading />;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 ">
-      <form className='w-2/3 mb-8' onSubmit={handleSubmit}>
-        <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Car Brand</label>
-        <input value={name} onChange={(e)=>setName(e.target.value)} type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
-        <label htmlFor="url" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">urlt</label>
-        <input type="text" id="url" value={url} onChange={(e)=> setUrl(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
-        <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company Description</label>
-        <textarea id="description"  value={description} onChange={(e)=> setDescription(e.target.value)}className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-3" placeholder="Leave a comment..." required/>
-        <button type="submit" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">save</button>
+      <form className="w-2/3 mb-8" onSubmit={handleSubmit}>
+        <label
+          htmlFor="name"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Car Brand
+        </label>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          id="name"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+        />
+        <label
+          htmlFor="url"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          urlt
+        </label>
+        <input
+          type="text"
+          id="url"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+        />
+        <label
+          htmlFor="description"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Company Description
+        </label>
+        <textarea
+          id="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-3"
+          placeholder="Leave a comment..."
+          required
+        />
+        <button
+          type="submit"
+          className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        >
+          save
+        </button>
       </form>
       <div className="grid grid-cols-4 gap-12">
-      {data?.map((user)=>(
-        <>
-         <Card title={user.name} subTitle={user.url} className="max-w-5rem col-span-4 md:col-span-2 sm:col-span-1" key={user.id}>
-            <p className="m-0">
-              {user.descriptions}
-            </p>
-         </Card>
-        </>
-      ))}
+        {data?.map((user) => (
+          <>
+            <Card
+              title={user.name}
+              subTitle={user.url}
+              className="max-w-5rem col-span-4 md:col-span-2 sm:col-span-1"
+              key={user.id}
+            >
+              <p className="m-0">{user.descriptions}</p>
+            </Card>
+          </>
+        ))}
       </div>
     </main>
-  )
+  );
 }
 
 function Loading() {
   return (
-  <React.Fragment>
-    <main className="min-h-screen min-w-screen flex-col items-center justify-between p-8 grid grid-cols-4 gap-4 sm:gap-1 md:gap-2">
-        {Array.from({length: 5}, (_, i) => i).map(i =>(
-          <div className="w-[200px] h-[200px]  bg-slate-800"  key={i}>
-            <Skeleton className="w-[100px] h-[20px] rounded-full" />
+    <React.Fragment>
+      <main className="min-h-screen min-w-screen flex-col items-center justify-between p-8 grid grid-cols-4 gap-4 sm:gap-1 md:gap-2">
+        {Array.from({ length: 12 }, (_, i) => i + 1).map((id) => (
+          <div
+            className="w-[200px] h-[200px] col-span-4 md:col-span-2 lg:col-span-1 bg-slate-500"
+            key={id}
+          >
+            <Skeleton className="h-6 w-1/2 m-1"></Skeleton>
+            <Skeleton className="h-6 w-1/4 m-2"></Skeleton>
+            <Skeleton className="w-2/3 h-2/3 rounded-md m-2"></Skeleton>
           </div>
         ))}
-    </main>
-  </React.Fragment>
+      </main>
+    </React.Fragment>
   );
 }
